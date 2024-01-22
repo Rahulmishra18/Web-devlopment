86a8c402fa166f225bb64a74cc77ee784e969380
Rahulmishra18 <153306479+Rahulmishra18@users.noreply.github.com>
2024-01-22 18:09:38 +0530

    Co-authored-by: Rahulmishra18 <153306479+Rahulmishra18@users.noreply.github.com>

---
+++ java script.js/App.js
@@ -0,0 +1,16 @@
+import './App.page 2';
+import Welcome from './components/Welcome';
+
+function App()
+{
+    return {
+        <div className="App">
+          <Welcome name = "Pratiksha"></Welcome>
+          <Welcome name = "Pallavi"></Welcome>
+          <Welcome name = "Jay"></Welcome>
+          </div>
+    };
+}
+
+
+export default App;
---
+++ java script.js/const passwordInput = document.js
@@ -0,0 +1,8 @@
+const passwordInput = document.getElementById('password');
+const strengthIndicator = document.getElementById('strengthIndicator');
+
+passwordInput.addEventListener('input', updatePasswordStrength);
+
+function updatePasswordStrength() {
+  // Implement password strength logic and update strengthIndicator.textContent accordingly
+}
